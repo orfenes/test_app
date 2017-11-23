0a9b91948a8b69145da6138354206ef636de4d7e
Rodrigo Castro <rfocastro@sp.r7.com>
2017-11-23 20:14:45 -0200

    configurando factory_bot

---
+++ app/models/customer.rb
@@ -0,0 +1,6 @@
+class Customer < ApplicationRecord
+
+  def  full_name
+    "Sr. #{name}"
+  end
+end
---
+++ spec/factories/customer.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :customer do
+    name  'Rodrigo'
+    email 'rodrigo@orfenes.com'
+  end
+end
